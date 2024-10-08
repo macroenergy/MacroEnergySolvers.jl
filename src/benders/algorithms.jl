290e23059fe1735d8cb353bd50e4a45de759c987
JZL <jl8975@princeton.edu>
2024-10-08 01:17:31 -0400

    Just whitespace

--- src/benders/algorithms.jl
+++ src/benders/algorithms.jl
@@ -2,18 +2,18 @@ using Debugger
 
 function benders(planning_problem::Model,linking_variables::Vector{String},subproblems::DistributedArrays.DArray,linking_variables_sub::Dict)
 	
-    #### Algorithm from:
-    ### Pecci, F. and Jenkins, J. D. “Regularized Benders Decomposition for High Performance Capacity Expansion Models”. arXiv:2403.02559 [math]. URL: http://arxiv.org/abs/2403.02559.
+	#### Algorithm from:
+	### Pecci, F. and Jenkins, J. D. “Regularized Benders Decomposition for High Performance Capacity Expansion Models”. arXiv:2403.02559 [math]. URL: http://arxiv.org/abs/2403.02559.
 
 	### It's a regularized version of the Benders decomposition algorithm in:
 	### A. Jacobson, F. Pecci, N. Sepulveda, Q. Xu, and J. Jenkins, “A computationally efficient Benders decomposition for energy systems planning problems with detailed operations and time-coupling constraints.” INFORMS Journal on Optimization 6(1):32-45. doi: https://doi.org/10.1287/ijoo.2023.0005
 
 	## Start solver time
 	solver_start_time = time()
-    
-    #### Algorithm parameters:
+	
+	#### Algorithm parameters:
 	MaxIter = 300;
-    ConvTol = 1e-3;
+	ConvTol = 1e-3;
 	MaxCpuTime = 3600;
 	γ = 0.5;
 	stab_method ="int_level_set";
@@ -23,7 +23,7 @@ function benders(planning_problem::Model,linking_variables::Vector{String},subpr
 		throw(DomainError(multicut_removal_method, "Invalid multicut_removal_method"))
 	end
 
-    integer_investment = false;
+	integer_investment = false;
 	# stab_method = "dynamic"; #dynamic or fixed 
 	cut_selection_method = "norm";
 	#############	
@@ -39,17 +39,17 @@ function benders(planning_problem::Model,linking_variables::Vector{String},subpr
 		integer_routine_flag = true;
 	end
 
-    #### Initialize UB and LB
+	#### Initialize UB and LB
 	planning_sol = solve_planning_problem(planning_problem,linking_variables);
 
-    UB = Inf;
-    LB = planning_sol.LB;
+	UB = Inf;
+	LB = planning_sol.LB;
 
-    LB_hist = Float64[];
-    UB_hist = Float64[];
+	LB_hist = Float64[];
+	UB_hist = Float64[];
 	 # TODO add type, and add optionality for when not debugging
 	 multicut_hists = [];
-    cpu_time = Float64[];
+	cpu_time = Float64[];
 
 	subop_sol_hist = Vector{Dict{Any, Any}}()
 
@@ -58,13 +58,13 @@ function benders(planning_problem::Model,linking_variables::Vector{String},subpr
 	# TODO add types, then update function format below
 	constraints_dict_tocheck_binding = Dict()
 
-    #### Run Benders iterations
-    for k = 1:MaxIter
+	#### Run Benders iterations
+	for k = 1:MaxIter
 		
 		start_subop_sol = time();
 
-        subop_sol = solve_dist_subproblems(subproblems,planning_sol);
-        
+		subop_sol = solve_dist_subproblems(subproblems,planning_sol);
+		
 		cpu_subop_sol = time()-start_subop_sol;
 		println("Solving the subproblems required $cpu_subop_sol seconds")
 
@@ -103,18 +103,18 @@ function benders(planning_problem::Model,linking_variables::Vector{String},subpr
 		# For multiCut_added constraints, if dual is = 0, then is binding, so
 		# add to multiCut_added constraints_dict_tocheck_binding's count
 		# If non-binding, reset count
-        for (nm, con_dual) in unst_planning_sol.GTE_cons_names_dual
-            # TODO abs?, within thresh?
+		for (nm, con_dual) in unst_planning_sol.GTE_cons_names_dual
+			# TODO abs?, within thresh?
 			# Non binding, accumulate
-            if abs(con_dual) == 0
-                constraints_dict_tocheck_binding[nm][2] += 1
+			if abs(con_dual) == 0
+				constraints_dict_tocheck_binding[nm][2] += 1
 			# Reset count
 			else
-                constraints_dict_tocheck_binding[nm][2] = 0
+				constraints_dict_tocheck_binding[nm][2] = 0
 			end
 			# Keep track of dual for later plotting
 			constraints_dict_tocheck_binding[nm][3] = con_dual
-        end
+		end
 		
 		append!(LB_hist, LB)
 		append!(UB_hist, UB)
@@ -123,12 +123,12 @@ function benders(planning_problem::Model,linking_variables::Vector{String},subpr
 
 
 		if any(subop_sol[w].theta_coeff==0 for w in keys(subop_sol))
-			println("***k = ", k,"      LB = ", LB,"     UB = ", UB,"       Gap = ", (UB-LB)/abs(LB),"       CPU Time = ",cpu_time[end])
+			println("***k = ", k,"	  LB = ", LB,"	 UB = ", UB,"	   Gap = ", (UB-LB)/abs(LB),"	   CPU Time = ",cpu_time[end])
 		else
-			println("k = ", k,"      LB = ", LB,"     UB = ", UB,"       Gap = ", (UB-LB)/abs(LB),"       CPU Time = ",cpu_time[end])
+			println("k = ", k,"	  LB = ", LB,"	 UB = ", UB,"	   Gap = ", (UB-LB)/abs(LB),"	   CPU Time = ",cpu_time[end])
 		end
 
-        if (UB-LB)/abs(LB) <= ConvTol
+		if (UB-LB)/abs(LB) <= ConvTol
 			if integer_routine_flag
 				println("*** Switching on integer constraints *** ")
 				UB = Inf;
@@ -178,7 +178,7 @@ function benders(planning_problem::Model,linking_variables::Vector{String},subpr
 					for v in binary_variables
 						fix(v,unst_planning_sol.values[name(v)];force=true)
 					end
-                    println("Solving the interior level set problem with γ = $γ")
+					println("Solving the interior level set problem with γ = $γ")
 
 					planning_sol = solve_int_level_set_problem(planning_problem,linking_variables,unst_planning_sol,LB,UB,γ);
 					unfix.(integer_variables)
@@ -188,7 +188,7 @@ function benders(planning_problem::Model,linking_variables::Vector{String},subpr
 					set_lower_bound.(integer_variables,0.0)
 					set_lower_bound.(binary_variables,0.0)
 				else
-                    println("Solving the interior level set problem with γ = $γ")
+					println("Solving the interior level set problem with γ = $γ")
 					planning_sol = solve_int_level_set_problem(planning_problem,linking_variables,unst_planning_sol,LB,UB,γ);
 				end
 				cpu_stab_method = time()-start_stab_method;
@@ -199,9 +199,9 @@ function benders(planning_problem::Model,linking_variables::Vector{String},subpr
 
 		end
 
-    end
+	end
 
-    return (planning_problem=planning_problem, planning_sol=planning_sol_best,
+	return (planning_problem=planning_problem, planning_sol=planning_sol_best,
 			LB_hist=LB_hist, UB_hist=UB_hist, multicut_hists=multicut_hists,
 			cpu_time=cpu_time, subop_sol_hist=subop_sol_hist)
 end
@@ -238,78 +238,78 @@ function update_planning_problem_multi_cuts!(m::Model,subop_sol::Dict,
 			if conDictVal[2] > REPEATED_NONBINDING_THRESH
 
 				if multicut_removal_method == :repeated_nonbinding
-                    println("DELETING CON: Repeated Nonbinding")
+					println("DELETING CON: Repeated Nonbinding")
 					delete(m, conDictVal[1])
 					delete!(constraints_dict_tocheck_binding, conName)
 				end	
 			end
 		end
 		for w in W
-            avoid = false
-            # QUESTION: 1:(k-1) or just check previous. Then why for loop?
-            # QUESTION: why do I need to use MacroEnergySystemsDecomposition.relVector
+			avoid = false
+			# QUESTION: 1:(k-1) or just check previous. Then why for loop?
+			# QUESTION: why do I need to use MacroEnergySystemsDecomposition.relVector
 
-            # TODO - doesn't shortcut early with this vectorization. Could be slower?
-            # Note: `Ref` forces only to vectorize on first argument
-            min_dist_lambda = minimum(
-                MacroEnergySystemsDecomposition.relVector.(
-                    [subop_sol_hist[j][w].lambda for j in 1:(k-1)],
-                    Ref(subop_sol_hist[k][w].lambda))
-            )
-            min_dist_op_cost = minimum(
-                MacroEnergySystemsDecomposition.relVector.(
-                    [subop_sol_hist[j][w].op_cost for j in 1:(k-1)],
-                    Ref(subop_sol_hist[k][w].op_cost))
-            )
+			# TODO - doesn't shortcut early with this vectorization. Could be slower?
+			# Note: `Ref` forces only to vectorize on first argument
+			min_dist_lambda = minimum(
+				MacroEnergySystemsDecomposition.relVector.(
+					[subop_sol_hist[j][w].lambda for j in 1:(k-1)],
+					Ref(subop_sol_hist[k][w].lambda))
+			)
+			min_dist_op_cost = minimum(
+				MacroEnergySystemsDecomposition.relVector.(
+					[subop_sol_hist[j][w].op_cost for j in 1:(k-1)],
+					Ref(subop_sol_hist[k][w].op_cost))
+			)
 
-            logDict_perW["cut_dist_"*string(w)] = (; min_dist_lambda, min_dist_op_cost)
+			logDict_perW["cut_dist_"*string(w)] = (; min_dist_lambda, min_dist_op_cost)
 
 			TOO_CLOSE_DIST_THRESH = 0.2
-            if multicut_removal_method == :similar_dist  &&
-               min_dist_lambda  <= TOO_CLOSE_DIST_THRESH &&
+			if multicut_removal_method == :similar_dist  &&
+			   min_dist_lambda  <= TOO_CLOSE_DIST_THRESH &&
 			   min_dist_op_cost <= TOO_CLOSE_DIST_THRESH
 
-                println("DELETING CON: Too similar")
-                avoid = true
-            end
-            if avoid == false
-                newCon = @constraint(m,
-                    # QUESTION: if within the `w` for loop, only want to add this specific w?
-                    # Could also collect the non-avoid W's and `intersect` that list with W?
-                    # [w in W],
-                    subop_sol[w].theta_coeff * m[:vTHETA][w] >=
-                    subop_sol[w].op_cost +
-                    sum(
-                        subop_sol[w].lambda[i] *
-                        (variable_by_name(m, linking_variables_sub[w][i]) -
-                         planning_sol.values[linking_variables_sub[w][i]])
-                        for i in 1:length(linking_variables_sub[w])))
+				println("DELETING CON: Too similar")
+				avoid = true
+			end
+			if avoid == false
+				newCon = @constraint(m,
+					# QUESTION: if within the `w` for loop, only want to add this specific w?
+					# Could also collect the non-avoid W's and `intersect` that list with W?
+					# [w in W],
+					subop_sol[w].theta_coeff * m[:vTHETA][w] >=
+					subop_sol[w].op_cost +
+					sum(
+						subop_sol[w].lambda[i] *
+						(variable_by_name(m, linking_variables_sub[w][i]) -
+						 planning_sol.values[linking_variables_sub[w][i]])
+						for i in 1:length(linking_variables_sub[w])))
 
-                this_con_name = "multiCut_added_" * string(k) * "_" * string(w)
-                set_name(newCon, this_con_name)
-                # (con, number of times repeated nonbinding, last dual value)
-                constraints_dict_tocheck_binding[this_con_name] = [newCon, 0, NaN]
+				this_con_name = "multiCut_added_" * string(k) * "_" * string(w)
+				set_name(newCon, this_con_name)
+				# (con, number of times repeated nonbinding, last dual value)
+				constraints_dict_tocheck_binding[this_con_name] = [newCon, 0, NaN]
 				end   
 		end
 	else 
-        firstRoundCons = @constraint(m,
-            [w in W],
-            subop_sol[w].theta_coeff * m[:vTHETA][w] >=
-            subop_sol[w].op_cost +
-            sum(
-                subop_sol[w].lambda[i] *
-                (variable_by_name(m, linking_variables_sub[w][i]) -
-                 planning_sol.values[linking_variables_sub[w][i]])
-                for i in 1:length(linking_variables_sub[w])
-            ))
-        for (this_w, con) in enumerate(firstRoundCons)
-            this_con_name = "multiCut_added_" * string(k) * "_" * string(this_w)
-            # (con, number of times repeated nonbinding, last dual value)
-            constraints_dict_tocheck_binding[this_con_name] = [con, 0, NaN]
-            set_name(con, this_con_name)
-        end
-    end
-    return (; logDict_perW, constraints_dict_tocheck_binding)
+		firstRoundCons = @constraint(m,
+			[w in W],
+			subop_sol[w].theta_coeff * m[:vTHETA][w] >=
+			subop_sol[w].op_cost +
+			sum(
+				subop_sol[w].lambda[i] *
+				(variable_by_name(m, linking_variables_sub[w][i]) -
+				 planning_sol.values[linking_variables_sub[w][i]])
+				for i in 1:length(linking_variables_sub[w])
+			))
+		for (this_w, con) in enumerate(firstRoundCons)
+			this_con_name = "multiCut_added_" * string(k) * "_" * string(this_w)
+			# (con, number of times repeated nonbinding, last dual value)
+			constraints_dict_tocheck_binding[this_con_name] = [con, 0, NaN]
+			set_name(con, this_con_name)
+		end
+	end
+	return (; logDict_perW, constraints_dict_tocheck_binding)
 
 end
 
--- src/benders/planning_problem.jl
+++ src/benders/planning_problem.jl
@@ -1,25 +1,25 @@
 function initialize_planning_problem(system::Any,model_type::Symbol)
 
-    if model_type==:MACRO
-        planning_problem,linking_variables = Macro.generate_planning_problem(system);
-    end
+	if model_type==:MACRO
+		planning_problem,linking_variables = Macro.generate_planning_problem(system);
+	end
 
-    planning_optimizer = optimizer_with_attributes(()->Gurobi.Optimizer(GRB_ENV[]),"Method"=>2,"BarConvTol"=>1e-3,"Crossover"=>0,"MIPGap"=>1e-3)
+	planning_optimizer = optimizer_with_attributes(()->Gurobi.Optimizer(GRB_ENV[]),"Method"=>2,"BarConvTol"=>1e-3,"Crossover"=>0,"MIPGap"=>1e-3)
 
-    set_optimizer(planning_problem,planning_optimizer)
+	set_optimizer(planning_problem,planning_optimizer)
 
-    set_silent(planning_problem)
+	set_silent(planning_problem)
 
-    return planning_problem,linking_variables
+	return planning_problem,linking_variables
 
 end
 
 function solve_planning_problem(m::Model,linking_variables::Vector{String})
 	
 	
-    optimize!(m)
+	optimize!(m)
 
-    if has_values(m) #
+	if has_values(m) #
 		# While at the thread where `optimize!`, capture the benders cuts'
 		# duals, so can tell if is binding
 		# These `multiCut_added_` (benders cuts) constraints are all GreaterThan
@@ -32,29 +32,29 @@ function solve_planning_problem(m::Model,linking_variables::Vector{String})
 			name.(GTE_cons), dual.(GTE_cons)
 		))
 		# Filter to only multicut_added named ones
-        GTE_cons_names_dual = filter(tup -> startswith(tup[1], "multiCut_added_"),
+		GTE_cons_names_dual = filter(tup -> startswith(tup[1], "multiCut_added_"),
 									GTE_cons_names_dual)
 
-        planning_sol = (
-            LB=objective_value(m),
-            inv_cost=value(m[:eFixedCost]),
-            values=Dict([s => value.(variable_by_name(m, s)) for s in linking_variables]),
+		planning_sol = (
+			LB=objective_value(m),
+			inv_cost=value(m[:eFixedCost]),
+			values=Dict([s => value.(variable_by_name(m, s)) for s in linking_variables]),
 			theta=value.(m[:vTHETA]),
 			GTE_cons_names_dual = GTE_cons_names_dual
 			)
-    else
-        compute_conflict!(m)
-        list_of_conflicting_constraints = ConstraintRef[];
-        for (F, S) in list_of_constraint_types(m)
-            for con in all_constraints(m, F, S)
-                if get_attribute(con, MOI.ConstraintConflictStatus()) == MOI.IN_CONFLICT
-                    push!(list_of_conflicting_constraints, con)
-                end
-            end
-        end
-        display(list_of_conflicting_constraints)
-        @error "The planning solution failed. This should not happen"
-    end
+	else
+		compute_conflict!(m)
+		list_of_conflicting_constraints = ConstraintRef[];
+		for (F, S) in list_of_constraint_types(m)
+			for con in all_constraints(m, F, S)
+				if get_attribute(con, MOI.ConstraintConflictStatus()) == MOI.IN_CONFLICT
+					push!(list_of_conflicting_constraints, con)
+				end
+			end
+		end
+		display(list_of_conflicting_constraints)
+		@error "The planning solution failed. This should not happen"
+	end
 
-    return planning_sol
+	return planning_sol
 end
